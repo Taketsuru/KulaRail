50489618bf7dc0950a2741c7dfbf41ee596ec270
Taketsuru11 <taketsuru11@gmail.com>
2012-07-04 12:37:45 +0900

    Implement inter-world minecart teleport

--- src/com/github/taketsuru/KulaRail/SignedBlockManager.java
+++ src/com/github/taketsuru/KulaRail/SignedBlockManager.java
@@ -21,168 +21,188 @@ import org.bukkit.plugin.Plugin;
 
 public class SignedBlockManager implements Listener {
 
-    public static Block getAttachedBlock(Block signBlock) {
-	org.bukkit.material.Sign data = new org.bukkit.material.Sign(signBlock.getType(), signBlock.getData());
-	return signBlock.getRelative(data.getAttachedFace());
-    }
-    
-    public SignedBlock getSignedBlockAt(Location location) {
-	return signedBlocks.get(location);
-    }
+	public static Block getAttachedBlock(Block signBlock) {
+		org.bukkit.material.Sign data =
+				new org.bukkit.material.Sign(signBlock.getType(),
+											 signBlock.getData());
+		return signBlock.getRelative(data.getAttachedFace());
+	}
 
-    public void listen(SignedBlockListener listener) {
-	listeners.add(listener);
-    }
+	public SignedBlock getSignedBlockAt(Location location) {
+		return signedBlocks.get(location);
+	}
 
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-	Location blockLocation = event.getBlock().getLocation();
-	SignedBlock block = getSignedBlockAt(blockLocation);
-	if (block == null) {
-	    Material type = event.getBlock().getType();
-	    if (type == Material.SIGN_POST || type == Material.WALL_SIGN) {
-		blockLocation = getAttachedBlock(event.getBlock()).getLocation();
-		block = getSignedBlockAt(blockLocation);
-		if (block != null && ! block.getSignBlock().equals(event.getBlock())) {
-		    block = null;
+	public void listen(SignedBlockListener listener) {
+		listeners.add(listener);
+	}
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		Location blockLocation = event.getBlock().getLocation();
+		SignedBlock block = getSignedBlockAt(blockLocation);
+		if (block == null) {
+			Material type = event.getBlock().getType();
+			if (type == Material.SIGN_POST || type == Material.WALL_SIGN) {
+				blockLocation =
+					getAttachedBlock(event.getBlock()).getLocation();
+				block = getSignedBlockAt(blockLocation);
+				if (block != null
+					&& ! block.getSignBlock().equals(event.getBlock())) {
+					block = null;
+				}
+			}
 		}
-	    }
-	}
-	if (block == null) {
-	    return;
-	}
-
-	onDestroy(block);
-    }
-
-    void onDestroy(SignedBlock block) {
-	for (SignedBlockListener listener : listeners) {
-	    listener.onDestroy(block);
-	}
-
-	signedBlocks.remove(block.getLocation());
-	
-	Chunk chunk = block.getBlock().getChunk();
-	Integer count = countInEachChunk.get(chunk);
-	if (count == 1) {
-	    countInEachChunk.remove(chunk);
-	    chunk.unload(true, true);
-	} else {
-	    countInEachChunk.put(chunk, count - 1);
-	}
-    }
-
-    public void onDisable(Plugin plugin) {
-	for (Iterator<Chunk> iter = countInEachChunk.keySet().iterator(); iter.hasNext(); ) {
-	    Chunk chunk = iter.next();
-	    iter.remove();
-	    chunk.unload(true, true);
-	}
-
-	Vector<Object> listData = new Vector<Object>();
-	for (SignedBlock entry : signedBlocks.values()) {
-	    HashMap<String, Object> state = new HashMap<String, Object>();
-	    entry.saveState(state);
-	    listData.add(state);
-	}
-	plugin.getConfig().set("signed_blocks", listData);
-    }
-
-    public void onEnable(Plugin plugin) {
-	signedBlocks.clear();
-
-	for (SignedBlockListener listener : listeners) {
-	    listener.onReset();
-	}
-
-	if (plugin.getConfig().isList("signed_blocks")) {
-	    List<Map<?, ?>> listData = plugin.getConfig().getMapList("signed_blocks");
-	    for (Map<?, ?> listDataEntry : listData) {
-		try {
-		    onCreate(new SignedBlock(plugin.getServer(), listDataEntry));
-		} catch (Exception e) {
-		    log.info("recovered");
-		    e.printStackTrace();
+		if (block == null) {
+			return;
 		}
-	    }
-	}
-	log.info(String.format("%d signed blocks are created", signedBlocks.size()));
 
-	plugin.getServer().getPluginManager().registerEvents(this, plugin);
-    }
-
-    @EventHandler
-    public void onSignChange(SignChangeEvent event) {
-	if (event.isCancelled()) {
-	    return;
+		onDestroy(block);
 	}
 
-	Block signBlock = event.getBlock();
-	Block selfBlock = getAttachedBlock(signBlock);
-	if (signedBlocks.get(selfBlock.getLocation()) != null) {
-	    return;
-	}
+	void onDestroy(SignedBlock block) {
+		for (SignedBlockListener listener : listeners) {
+			listener.onDestroy(block);
+		}
 
-	SignedBlock block = new SignedBlock(selfBlock.getLocation(), signBlock.getLocation(), event.getLines());
-	Player player = event.getPlayer();
-	if (! mayCreate(player, block)) {
-	    player.sendMessage("ERROR: Ç±ÇÃä≈î¬ÇçÏÇÈå†å¿Ç™Ç†ÇËÇ‹ÇπÇÒ");
-	    event.setCancelled(true);
-	    return;
-	}
+		signedBlocks.remove(block.getLocation());
 
-	if (onCreate(block)) {
-	    String[] signText = block.getSignText();
-	    for (int i = 0; i < signText.length; ++i) {
-		event.setLine(i, signText[i]);
-	    }
-	    block.setIsChangingSign(false);
-	}
-    }
-
-    @EventHandler
-    public void onChunkUnload(ChunkUnloadEvent event) {
-	if (event.isCancelled()) {
-	    return;
-	}
-	
-	Chunk chunk = event.getChunk();
-	if (countInEachChunk.containsKey(chunk) && 0 < countInEachChunk.get(chunk)) {
-	    event.setCancelled(true);
-	}
-    }
-
-    public void unlisten(SignedBlockListener listener) {
-	listeners.remove(listener);
-    }
-
-    boolean mayCreate(Player player, SignedBlock block) {
-	for (SignedBlockListener listener : listeners) {
-	    if (! listener.mayCreate(player, block)) {
-		return false;
-	    }
-	}
-	return true;
-    }
-
-    boolean onCreate(SignedBlock block) {
-	for (SignedBlockListener listener : listeners) {
-	    if (listener.onCreate(block)) {
-		signedBlocks.put(block.getLocation(), block);
 		Chunk chunk = block.getBlock().getChunk();
-		if (! countInEachChunk.containsKey(chunk)) {
-		    countInEachChunk.put(chunk, 1);
+		Integer count = countInEachChunk.get(chunk);
+		if (count == 1) {
+			countInEachChunk.remove(chunk);
+			chunk.unload(true, true);
 		} else {
-		    countInEachChunk.put(chunk, countInEachChunk.get(chunk) + 1);
+			countInEachChunk.put(chunk, count - 1);
+		}
+	}
+
+	public void onDisable(Plugin plugin) {
+		for (Iterator<Chunk> iter = countInEachChunk.keySet().iterator();
+			 iter.hasNext(); ) {
+			Chunk chunk = iter.next();
+			iter.remove();
+			chunk.unload(true, true);
+		}
+
+		Vector<Object> listData = new Vector<Object>();
+		for (SignedBlock entry : signedBlocks.values()) {
+			HashMap<String, Object> state = new HashMap<String, Object>();
+			entry.saveState(state);
+			listData.add(state);
+		}
+		plugin.getConfig().set("signed_blocks", listData);
+
+		this.plugin = null;
+	}
+
+	public void onEnable(Plugin plugin) {
+		this.plugin = plugin;
+
+		signedBlocks.clear();
+
+		for (SignedBlockListener listener : listeners) {
+			listener.onReset();
+		}
+
+		if (plugin.getConfig().isList("signed_blocks")) {
+			List<Map<?, ?>> listData =
+				plugin.getConfig().getMapList("signed_blocks");
+			for (Map<?, ?> listDataEntry : listData) {
+				try {
+					onCreate(new SignedBlock(plugin.getServer(),
+											 listDataEntry));
+				} catch (Exception e) {
+					log.info("recovered");
+					e.printStackTrace();
+				}
+			}
+		}
+		log.info(String.format("%d signed blocks are created", signedBlocks.size()));
+
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+	@EventHandler
+	public void onSignChange(SignChangeEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+
+		Block signBlock = event.getBlock();
+		Block selfBlock = getAttachedBlock(signBlock);
+		if (signedBlocks.get(selfBlock.getLocation()) != null) {
+			return;
+		}
+
+		SignedBlock block =
+				new SignedBlock(selfBlock.getLocation(),
+								signBlock.getLocation(), event.getLines());
+		Player player = event.getPlayer();
+		if (! mayCreate(player, block)) {
+			player.sendMessage("ERROR: Ç±ÇÃä≈î¬ÇçÏÇÈå†å¿Ç™Ç†ÇËÇ‹ÇπÇÒ");
+			event.setCancelled(true);
+			return;
+		}
+
+		if (onCreate(block)) {
+			String[] signText = block.getSignText();
+			for (int i = 0; i < signText.length; ++i) {
+				event.setLine(i, signText[i]);
+			}
+			block.setIsChangingSign(false);
+		}
+	}
+
+	@EventHandler
+	public void onChunkUnload(ChunkUnloadEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+
+		final Chunk chunk = event.getChunk();
+		if (countInEachChunk.containsKey(chunk) && 0 < countInEachChunk.get(chunk)) {
+			event.setCancelled(true);
+			plugin.getServer().getScheduler()
+			.scheduleSyncDelayedTask(plugin, new Runnable() {
+				public void run() {
+					chunk.load(false);
+				}
+			}, 1);
+		}
+	}
+
+	public void unlisten(SignedBlockListener listener) {
+		listeners.remove(listener);
+	}
+
+	boolean mayCreate(Player player, SignedBlock block) {
+		for (SignedBlockListener listener : listeners) {
+			if (! listener.mayCreate(player, block)) {
+				return false;
+			}
 		}
 		return true;
-	    }
 	}
-	return false;
-    }
 
-    Vector<SignedBlockListener> listeners = new Vector<SignedBlockListener>();
-    Logger log = Logger.getLogger("Minecraft");
-    HashMap<Location, SignedBlock> signedBlocks = new HashMap<Location, SignedBlock>();
-    HashMap<Chunk, Integer> countInEachChunk = new HashMap<Chunk, Integer>(); 
+	boolean onCreate(SignedBlock block) {
+		for (SignedBlockListener listener : listeners) {
+			if (listener.onCreate(block)) {
+				signedBlocks.put(block.getLocation(), block);
+				Chunk chunk = block.getBlock().getChunk();
+				if (! countInEachChunk.containsKey(chunk)) {
+					countInEachChunk.put(chunk, 1);
+				} else {
+					countInEachChunk.put(chunk, countInEachChunk.get(chunk) + 1);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Vector<SignedBlockListener> listeners = new Vector<SignedBlockListener>();
+	Logger log = Logger.getLogger("Minecraft");
+	Plugin plugin;
+	HashMap<Location, SignedBlock> signedBlocks = new HashMap<Location, SignedBlock>();
+	HashMap<Chunk, Integer> countInEachChunk = new HashMap<Chunk, Integer>(); 
 }
--- src/com/github/taketsuru/KulaRail/TeleporterManager.java
+++ src/com/github/taketsuru/KulaRail/TeleporterManager.java
@@ -4,11 +4,15 @@ import java.util.HashMap;
 import java.util.Vector;
 import java.util.logging.Logger;
 
+import net.minecraft.server.WorldServer;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Vehicle;
 import org.bukkit.event.EventHandler;
@@ -19,157 +23,189 @@ import org.bukkit.plugin.Plugin;
 
 public class TeleporterManager implements Listener, SignedBlockListener {
 
-    public static boolean isEntryBlock(SignedBlock block) {
-	return block.readHeader().equalsIgnoreCase(entryHeader);
+  public static boolean isEntryBlock(SignedBlock block) {
+    return block.readHeader().equalsIgnoreCase(entryHeader);
+  }
+
+  public static boolean isExitBlock(SignedBlock block) {
+    return block.readHeader().equalsIgnoreCase(exitHeader);
+  }
+
+  public TeleporterManager(SignedBlockManager signedBlockManager) {
+    this.signedBlockManager = signedBlockManager;
+    signedBlockManager.listen(this);
+  }
+
+  public void onEnable(Plugin plugin) {
+    plugin.getServer().getPluginManager().registerEvents(this, plugin);
+  }
+
+  @EventHandler
+  public void onVehicleBlockCollision(VehicleBlockCollisionEvent event) {
+    Block blockCollidedWith = event.getBlock();
+    SignedBlock entry = signedBlockManager
+      .getSignedBlockAt(blockCollidedWith.getLocation());
+    if (entry == null || !isEntryBlock(entry)) {
+      return;
     }
 
-    public static boolean isExitBlock(SignedBlock block) {
-	return block.readHeader().equalsIgnoreCase(exitHeader);
+    String channelName = entry.readLabel();
+    Vector<SignedBlock> channel = channels.get(channelName);
+    if (channel == null) {
+      return;
     }
 
-    public TeleporterManager(SignedBlockManager signedBlockManager) {
-	this.signedBlockManager = signedBlockManager;
-	signedBlockManager.listen(this);
+    Location exitLocation = null;
+    double minDistanceSquared = Double.MAX_VALUE;
+    for (SignedBlock block : channel) {
+      if (!isExitBlock(block)) {
+        continue;
+      }
+      double distanceSquared = 
+        block.getLocation().getWorld() == entry.getLocation().getWorld()
+        ? block.getLocation().distanceSquared(entry.getLocation())
+        : Double.MAX_VALUE / 2;
+      if (distanceSquared < minDistanceSquared) {
+        minDistanceSquared = distanceSquared;
+        exitLocation = block.getLocation();
+      }
+    }
+    if (exitLocation == null) {
+      return;
     }
 
-    public void onEnable(Plugin plugin) {
-	plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    Block exitBlock = exitLocation.getBlock();
+
+    BlockFace[] faces = { BlockFace.WEST, BlockFace.NORTH, BlockFace.EAST,
+                          BlockFace.SOUTH };
+    for (int dir = 0; dir < faces.length; ++dir) {
+      BlockFace face = faces[dir];
+      Block adjacent = exitBlock.getRelative(face);
+      BlockState state = adjacent.getState();
+      if (!(state.getData() instanceof org.bukkit.material.Rails)) {
+        continue;
+      }
+
+      Rails rails = (org.bukkit.material.Rails) state.getData();
+      boolean attached = false;
+      switch (rails.getDirection()) {
+      case EAST:
+      case WEST:
+        attached = face.equals(BlockFace.EAST)
+          || face.equals(BlockFace.WEST);
+        break;
+
+      case NORTH:
+      case SOUTH:
+        attached = face.equals(BlockFace.SOUTH)
+          || face.equals(BlockFace.NORTH);
+        break;
+
+      default:
+        break;
+      }
+
+      if (attached) {
+        Vehicle vehicle = event.getVehicle();
+        vehicle.eject();
+        Location newLocation =
+          exitLocation.clone().add(face.getModX() + 0.5, face.getModY() + 0.35,
+                                   face.getModZ() + 0.5);
+        newLocation.setYaw(dir * 90.0f);
+        if (newLocation.getWorld() == vehicle.getWorld()) {
+          vehicle.teleport(newLocation);
+        } else {
+          WorldServer sourceWorld =
+            ((CraftWorld)vehicle.getWorld()).getHandle();
+          WorldServer destWorld =
+            ((CraftWorld)newLocation.getWorld()).getHandle();
+          net.minecraft.server.Entity vehicleHandle =
+            ((CraftEntity)vehicle).getHandle();
+          sourceWorld.tracker.untrackEntity(vehicleHandle);
+          vehicleHandle.world.removeEntity(vehicleHandle);
+          vehicleHandle.dead = false;
+          vehicleHandle.world = destWorld;
+          vehicleHandle.setLocation(newLocation.getX(),
+                                    newLocation.getY(),
+                                    newLocation.getZ(),
+                                    newLocation.getYaw(),
+                                    newLocation.getPitch());
+          destWorld.addEntity(vehicleHandle);
+          destWorld.tracker.track(vehicleHandle);
+        }
+        vehicle.setVelocity(new org.bukkit.util.Vector());
+        break;
+      }
+    }
+  }
+
+  @Override
+  public void onReset() {
+    channels.clear();
+  }
+
+  @Override
+  public boolean mayCreate(Player player, SignedBlock block) {
+    if (!isEntryBlock(block) && !isExitBlock(block)) {
+      return true;
     }
 
-    @EventHandler
-    public void onVehicleBlockCollision(VehicleBlockCollisionEvent event) {
-	Block blockCollidedWith = event.getBlock();
-	SignedBlock entry = signedBlockManager.getSignedBlockAt(blockCollidedWith.getLocation());
-	if (entry == null || ! isEntryBlock(entry)) {
-	    return;
-	}
+    return player.hasPermission(isEntryBlock(block)
+				? "kularail.entry"
+				: "kularail.exit");
+  }
 
-	String channelName = entry.readLabel();
-	Vector<SignedBlock> channel = channels.get(channelName);
-	if (channel == null) {
-	    return;
-	}
-
-	Location exitLocation = null;
-	double minDistanceSquared = Double.MAX_VALUE;
-	for (SignedBlock block : channel) {
-	    if (! isExitBlock(block)) {
-		continue;
-	    }
-	    double distanceSquared = block.getLocation().distanceSquared(entry.getLocation());
-	    if (distanceSquared < minDistanceSquared) {
-		minDistanceSquared = distanceSquared;
-		exitLocation = block.getLocation();
-	    }
-	}
-	if (exitLocation == null) {
-	    return;
-	}
-
-	Block exitBlock = exitLocation.getBlock();
-
-	BlockFace[] faces = { BlockFace.WEST, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH};
-	for (int dir = 0; dir < faces.length; ++dir) {
-	    BlockFace face = faces[dir];
-	    Block adjacent = exitBlock.getRelative(face);
-	    BlockState state = adjacent.getState();
-	    if (! (state.getData() instanceof org.bukkit.material.Rails)) {
-		continue;
-	    }
-
-	    Rails rails = (org.bukkit.material.Rails)state.getData();
-	    boolean attached = false;
-	    switch (rails.getDirection()) {
-	    case EAST:
-	    case WEST:
-		attached = face.equals(BlockFace.EAST) || face.equals(BlockFace.WEST);
-		break;
-
-	    case NORTH:
-	    case SOUTH:
-		attached = face.equals(BlockFace.SOUTH) || face.equals(BlockFace.NORTH);
-		break;
-
-	    default:
-		break;
-	    }
-
-	    if (attached) {
-		Vehicle vehicle = event.getVehicle();
-		vehicle.eject();
-		Location newLocation = exitLocation.clone().add(face.getModX() + 0.5, face.getModY() + 0.35, face.getModZ() + 0.5);
-		newLocation.setYaw(dir * 90.0f);
-		vehicle.teleport(newLocation);
-		vehicle.setVelocity(new org.bukkit.util.Vector());
-		break;
-	    }
-	}
+  @Override
+  public boolean onCreate(SignedBlock block) {
+    if (!isEntryBlock(block) && !isExitBlock(block)) {
+      return false;
     }
-
-    @Override
-    public void onReset() {
-	channels.clear();
+    String name = block.readLabel();
+    Vector<SignedBlock> channel = channels.get(name);
+    if (channel == null) {
+      channel = new Vector<SignedBlock>();
+      channels.put(name, channel);
     }
+    channel.add(block);
+    updateColor(channel);
 
-    @Override
-    public boolean mayCreate(Player player, SignedBlock block) {
-	if (! isEntryBlock(block) && ! isExitBlock(block)) {
-	    return true;
-	}
+    return true;
+  }
 
-	return player.hasPermission(isEntryBlock(block) ? "kularail.entry" : "kularail.exit"); 
+  @Override
+  public void onDestroy(SignedBlock block) {
+    if (!isEntryBlock(block) && !isExitBlock(block)) {
+      return;
     }
-
-    @Override
-    public boolean onCreate(SignedBlock block) {
-	if (! isEntryBlock(block) && ! isExitBlock(block)) {
-	    return false;
-	}
-	String name = block.readLabel();
-	Vector<SignedBlock> channel = channels.get(name);
-	if (channel == null) {
-	    channel = new Vector<SignedBlock>();
-	    channels.put(name, channel);
-	}
-	channel.add(block);
-	updateColor(channel);
-	
-	return true;
-   }
-
-    @Override
-    public void onDestroy(SignedBlock block) {
-	if (! isEntryBlock(block) && ! isExitBlock(block)) {
-	    return;
-	}
-	String name = block.readLabel();
-	Vector<SignedBlock> channel = channels.get(name);
-	channel.remove(block);
-	if (channel.isEmpty()) {
-	    channels.remove(name);
-	}
-	updateColor(channel);
+    String name = block.readLabel();
+    Vector<SignedBlock> channel = channels.get(name);
+    channel.remove(block);
+    if (channel.isEmpty()) {
+      channels.remove(name);
     }
+    updateColor(channel);
+  }
 
-    void updateColor(Vector<SignedBlock> channel) {
-	int entryCount = 0;
-	for (SignedBlock block : channel) {
-	    if (isEntryBlock(block)) {
-		++entryCount;
-	    }
-	}
-	int exitCount = channel.size() - entryCount;
-
-	ChatColor exitColor = 0 < entryCount ? ChatColor.GREEN : ChatColor.RED;
-	ChatColor entryColor = 0 < exitCount ? ChatColor.GREEN : ChatColor.RED;
-	for (SignedBlock block : channel) {
-	    block.setHeaderColor(isEntryBlock(block) ? entryColor : exitColor);
-	}
+  void updateColor(Vector<SignedBlock> channel) {
+    int entryCount = 0;
+    for (SignedBlock block : channel) {
+      if (isEntryBlock(block)) {
+        ++entryCount;
+      }
     }
+    int exitCount = channel.size() - entryCount;
 
-    static final String entryHeader = "[kr.entry]";
-    static final String exitHeader = "[kr.exit]";
-    Logger log = Logger.getLogger("Minecraft");
-    SignedBlockManager signedBlockManager;
-    HashMap<String, Vector<SignedBlock>> channels = new HashMap<String, Vector<SignedBlock>>();
+    ChatColor exitColor = 0 < entryCount ? ChatColor.GREEN : ChatColor.RED;
+    ChatColor entryColor = 0 < exitCount ? ChatColor.GREEN : ChatColor.RED;
+    for (SignedBlock block : channel) {
+      block.setHeaderColor(isEntryBlock(block) ? entryColor : exitColor);
+    }
+  }
+
+  static final String entryHeader = "[kr.entry]";
+  static final String exitHeader = "[kr.exit]";
+  Logger log = Logger.getLogger("Minecraft");
+  SignedBlockManager signedBlockManager;
+  HashMap<String, Vector<SignedBlock>> channels =
+    new HashMap<String, Vector<SignedBlock>>();
 }
